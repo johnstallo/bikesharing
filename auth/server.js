28d59fd687914172f383f4cd43d5234777f19416
John Stallo <john.stallo@live.com>
2017-06-21 17:53:37 -0700

    Added 'watch sync' for auth service

--- auth/server.js
+++ auth/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
   console.log("Received request");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!\n");
+  response.end("Hello, this is the Auth service\n");
 });
 
 server.listen(80);
